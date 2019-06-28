39267acddc2a29215e66da4780961ec101cc2a93
arm <moraveca@gmail.com>
2019-06-27 20:45:06 -0500

    changed names to Dev...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import LogIn from "./devComponents/LogIn";
-import DetailForm from "./devComponents/DetailForm";
-import Chat from "./devComponents/Chat";
+import DevLogIn from "./devComponents/DevLogIn";
+import DevDetailForm from "./devComponents/DevDetailForm";
+import DevChat from "./devComponents/DevChat";
 
 function App() {
   const [user, setUser] = useState({})
 
   return (
      <div>
-      <LogIn user={user} setUser={setUser}/>
-      <DetailForm user={user} />
-      <Chat user={user} />
+      <DevLogIn user={user} setUser={setUser}/>
+      <DevDetailForm user={user} />
+      <DevChat user={user} />
     </div>
   )
 }
